158889e1f1b38b520fcb806e2c40c83979b37097
ROBERTOALCANTARAHURTADO <l21530229@cancun.tecnm.mx>
2024-11-14 21:55:30 -0500

    Add files via upload

---
+++ Practicas_Roberto/practica4.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Practica 4</title>
+</head>
+<body>
+<?php
+$numero=1;
+//verificar si el numero es par o impar
+if ($numero % 2 == 0) {
+echo "el numero $numero es par.<br><br>";
+}
+else {
+echo "el numero $numero es impar.<br><br>";
+}
+
+?>
+</body>
+</html>
