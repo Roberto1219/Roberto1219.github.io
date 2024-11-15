fcd4dda6470809bbbf75d5a5be3edd2f434b6dfa
ROBERTOALCANTARAHURTADO <l21530229@cancun.tecnm.mx>
2024-11-14 21:55:51 -0500

    Add files via upload

---
+++ Practicas_Roberto/practica5.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<title>practica 5 </title>
+</head>
+<body>
+<?php
+
+
+$dia="Lunes";
+switch ($dia){
+case "Lunes":
+echo "Hoy es Lunes";
+break;
+case "Martes":
+echo "Hoy es Martes";
+break;
+case "Miercoles":
+echo "Hoy es Miercoles";
+break;
+case "Jueves":
+echo "Hoy es Jueves";
+break;
+case "Viernes":
+echo "Hoy es Viernes";
+break;
+case "Sabado":
+echo "Hoy es Sabado";
+break;
+case "Domingo":
+echo "Hoy es Domingo";
+break;
+default:
+}
+
+
+?>
+</body>
+</html>
