2afe4289ae9113a86a4e196b28f3a38e78ad9a24
ROBERTOALCANTARAHURTADO <l21530229@cancun.tecnm.mx>
2024-11-14 21:56:21 -0500

    Add files via upload

---
+++ Practicas_Roberto/practica6.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title> Creacion de una base de datos </title>
+</head>
+<body>
+<?php
+require ("config.php");
+echo "<h1>Practica: conexion a mi servidor de datos MYSQL </h1><br><br>";
+if ($conexion=mysqli_connect($servidor, $usuario, $password, $bd))
+{
+echo "Tu conexion ha sido exitosa";
+}
+else
+{
+echo "Error conectando con MYSQL<br> ".mysqli_connect_error();
+}
+echo "<br><br>";
+mysqli_close($conexion);
+
+?>
+</body>
+</html>
